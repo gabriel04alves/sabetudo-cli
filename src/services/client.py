80471bbaf0ece063e8917e64035a72bd8f3623ed
Gabriel Alves <gabriel52.alves@gmail.com>
2025-11-08 18:10:03 -0300

    feat: update model in get_completion function and refine response prompt

--- src/services/client.py
+++ src/services/client.py
@@ -7,17 +7,18 @@ load_dotenv()
 API_KEY = os.getenv("GROQ_API_KEY")
 client = Groq(api_key=API_KEY)
 
+
 def get_completion(client, question):
     return client.chat.completions.create(
-        model="gemma2-9b-it",
+        model="llama-3.3-70b-versatile",
         messages=[
             {
                 "role": "system",
                 "content": f"""
                     Você é um assistente técnico de terminal, especializado em responder dúvidas sobre programação, comandos de sistema, ferramentas de desenvolvedor e tecnologia em geral.
-                    Responda de forma direta, objetiva e precisa à seguinte pergunta:
+                    Responda de forma curta, direta, objetiva e precisa à seguinte pergunta:
                     {question}
-                    """
+                    """,
             }
         ],
         temperature=0.7,
